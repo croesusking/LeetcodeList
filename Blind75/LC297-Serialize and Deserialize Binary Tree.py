759f112e9c006ea91695abf63adfdf9c501e3b8d
croesusking <64124465+croesusking@users.noreply.github.com>
2023-03-13 22:42:34 -0600

    LC297-Serialize and Deserialize Binary Tree

---
+++ Blind75/LC297-Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# LC 297 - Serialize and Deserialize Binary Tree
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        def dfs(root):
+            if not root:
+                res.append("None,")
+                return 
+            res.append(str(root.val)+",")
+            dfs(root.left)
+            dfs(root.right)
+            
+        res = []
+        dfs(root)
+        return "".join(res)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        def rdeserialize(l):
+            """ a recursive helper function for deserialization."""
+            if l[0] == 'None':
+                l.pop(0)
+                return None
+                
+            root = TreeNode(l[0])
+            l.pop(0)
+            root.left = rdeserialize(l)
+            root.right = rdeserialize(l)
+            return root
+
+        data_list = data.split(',')
+        root = rdeserialize(data_list)
+        return root
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
