34d591883b1e2b0ecd2a2dccaa4770d475fe11ed
Clara Chan <64124465+croesusking@users.noreply.github.com>
2023-04-14 21:25:55 -0600

    lc2218

---
+++ Daily LeetCode Challenge/lc2218.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int: 
+        #LC 2218 - Maximum Value of K Coins From Piles
+        n = len(piles)
+        dp = [[-1]* (k+1) for _ in range(n)]
+
+        def dfs(i,coins):
+            if i ==n:
+                return 0
+            if dp[i][coins]!=-1:
+                return dp[i][coins]
+            dp[i][coins] = dfs(i+1,coins) #we can choose to skip the current pile
+            curPile =0
+
+            for j in range(min(coins,len(piles[i]))):
+                curPile+=piles[i][j]
+                dp[i][coins] = max(dp[i][coins],curPile + dfs(i+1,coins-j-1))
+            return dp[i][coins]
+
+
+        return dfs(0,k)
